36b2cfa7b8939b6247cadaaf8a0a9bfccc3076d9
Anjali <anjalidelhi1509@gmail.com>
2023-01-09 12:32:02 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const app = express()
+const route = require("./routes/routes.js")
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+mongoose.connect("mongodb+srv://anjalis1509:anjalis1509@cluster0.rhdux0p.mongodb.net/group2Database", {
+    useNewUrlParser: true
+}).then(()=>console.log("MongoDb is connected"))
+.catch(err=>console.log(err))
+
+app.use("/", route)
+
+app.listen(process.env.PORT || 3000, ()=> {
+    console.log(`Server is running on port ${process.env.port||3000}`)
+})
